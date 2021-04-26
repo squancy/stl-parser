0c8014b1f243dc4427072224c1ece1f15e180da5
Mark Frankli <mark@pearscom.com>
2021-04-26 19:43:28 +0200

    binary stl validation unit tests

--- test/stlValidatorTests.c
+++ test/stlValidatorTests.c
@@ -22,12 +22,13 @@ int main(void) {
     "../test/stl/wrong_ascii_v2.stl",
     "../test/stl/wrong_ascii_v3.stl",
     "../test/stl/wrong_ascii_v4.stl",
-    "../test/stl/wrong_ascii_v5.stl"
+    "../test/stl/wrong_ascii_v5.stl",
+    "../test/stl/this_ascii_does_not_exist.stl"
   };
 
   // Expected output
   int output[] = {
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2
   };
   
   printf("Testing int validateSTLFile(char* fname, char type) in ASCII mode\n");
@@ -36,6 +37,32 @@ int main(void) {
     char* status = res == output[i] ? "passed" : "failed";
     printf("input file: %s, output: %d --> %s\n", fnames[i], res, status);
   } 
+
+  char* fnames2[] = {
+    "../test/stl/binary_v1.stl",
+    "../test/stl/binary_v2.stl",
+    "../test/stl/binary_v3.stl",
+    "../test/stl/binary_v4.stl",
+    "../test/stl/binary_v5.stl",
+    "../test/stl/binary_v6.stl",
+    "../test/stl/binary_v7.stl",
+    "../test/stl/binary_wrong_v1.stl",
+    "../test/stl/binary_wrong_v2.stl",
+    "../test/stl/binary_wrong_v3.stl",
+    "../test/stl/this_binary_does_not_exist.stl"
+  };
+
+  int output2[] = {
+    1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2
+  };
+
+  printf("\n");
+  printf("Testing int validateSTLFile(char* fname, char type) in binary mode\n");
+  for (i = 0; i < sizeof(output2) / sizeof(output2[0]); i++) {
+    int res = validateSTLFile(fnames2[i], 'b');
+    char* status = res == output2[i] ? "passed" : "failed";
+    printf("input file: %s, output: %d --> %s\n", fnames2[i], res, status);
+  }
 
   return 0;
 }
